02c4b9ff088227d8303f6302c451d72869ccc3da
hafizz7 <muh.hafizz80@gmail.com>
2023-10-24 08:00:58 +0800

    posttest 6

---
+++ lib/bottom_nav.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'Favorite.dart';
+import 'input_furniture_page.dart';
+import 'home_page.dart';
+import 'about.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  const BottomNavScreen();
+
+  @override
+  _BottomNavScreenState createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  int currentIndex = 0;
+
+  final List<Widget> screens = [
+    MyHomePage(),
+    Favorite_Page(),
+    Input_Page(),
+    About_Page(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentIndex],
+      bottomNavigationBar: CupertinoTabBar( // Mengganti BottomNavigationBar menjadi CupertinoTabBar
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home), // Mengganti ikon ke CupertinoIcons
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.heart), // Mengganti ikon ke CupertinoIcons
+            label: "Favorites",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.add), // Mengganti ikon ke CupertinoIcons
+            label: "Add",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person), // Mengganti ikon ke CupertinoIcons
+            label: "About",
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:furniture/bottom_nav.dart';
+import 'input_furniture_page.dart';
+import 'home_page.dart';
+import 'about.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/' :(context) => const MyHomePage(),
+        '/BottomNav' :(context) => const BottomNavScreen(),
+        '/my' :(context) => const MyApp(),
+        '/halaman2' : (context) => Input_Page(),
+        '/about' : (context) => const About_Page(),
+      },
+      initialRoute: '/BottomNav',
+    );
+  }
+}
